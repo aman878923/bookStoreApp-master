3c804abb44317e4f4f8d10269ce4f2f73117ed6d
aman agarwal <158491609+aman878923@users.noreply.github.com>
2024-12-04 18:17:17 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/bookStoreApp-master">
+  <BrowserRouter >
     <AuthProvider>
       <div className="dark:bg-slate-900 dark:text-white">
         <App />
