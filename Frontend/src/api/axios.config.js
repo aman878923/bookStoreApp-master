c38127281b3ff513fb3e2a8f85115e5be7d5fb25
aman878923 <aman.agarwal7385@gmail.com>
2024-12-08 16:28:08 +0530

    fix

--- Frontend/src/api/axios.config.js
+++ Frontend/src/api/axios.config.js
@@ -5,8 +5,24 @@ const api = axios.create({
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
+  // Add this line
 });
+// Add this before response interceptor
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 api.interceptors.response.use(
   (response) => response,
   (error) => {
