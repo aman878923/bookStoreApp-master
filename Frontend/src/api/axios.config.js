b26d6a168ec54168fd8936b0a22867f6f66ff33a
aman878923 <aman.agarwal7385@gmail.com>
2024-12-08 18:44:41 +0530

    gog

--- Frontend/src/api/axios.config.js
+++ Frontend/src/api/axios.config.js
@@ -10,28 +10,28 @@ const api = axios.create({
   // Add this line
 });
 // Add this before response interceptor
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
+api.interceptors.request.use(async (config) => {
+  if (!config.headers["X-CSRF-Token"]) {
+    const response = await axios.get(
+      "https://bookstoreapp-master.onrender.com/api/csrf-token",
+      {
+        withCredentials: true,
+      }
+    );
+    config.headers["X-CSRF-Token"] = response.data.csrfToken;
   }
-);
+  return config;
+});
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("user");
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
-  }
-);
+// api.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response?.status === 401) {
+//       localStorage.removeItem("user");
+//       window.location.href = "/login";
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 export default api;
