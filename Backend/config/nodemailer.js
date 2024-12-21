eb196f7a0ded17e0eb3a9c5fcc8cfeb43876bdba
aman878923 <aman.agarwal7385@gmail.com>
2024-12-21 14:37:07 +0530

    GO

--- Backend/config/nodemailer.js
+++ Backend/config/nodemailer.js
@@ -1,10 +1,14 @@
 import nodemailer from 'nodemailer';
+import dotenv from "dotenv";
 dotenv.config();
 
 export const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
 });
