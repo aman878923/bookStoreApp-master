5642afdaf41fc1d5dfafb919cefee6a561e6a7e5
aman878923 <aman.agarwal7385@gmail.com>
2024-12-21 15:23:46 +0530

    good

--- Backend/route/book.route.js
+++ Backend/route/book.route.js
@@ -12,8 +12,9 @@ import { authenticateJWT } from "../middleware/auth.js";
 const router = express.Router();
 
 router.get("/", getBook);
-router.get("/:id", getBookById);
 router.get("/search", searchBooks);
+router.get("/:id", getBookById);
+
 router.post("/:id/reviews", authenticateJWT, addReview); // Protected route
 router.put("/:id/reviews/:reviewId", authenticateJWT, updateReview); // Protected route
 router.delete("/:id/reviews/:reviewId", authenticateJWT, deleteReview); // Protected route
