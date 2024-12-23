8e989758bb98d374eb7c1e4537f9eaac3ac43e12
aman agarwal <158491609+aman878923@users.noreply.github.com>
2024-12-23 21:40:33 +0530

    token

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -129,7 +129,11 @@ export const login = async (req, res) => {
         fullname: user.fullname,
         email: user.email,
       },
+      
     });
+    localStorage.setItem("Token", token);
+    localStorage.setItem("Users", JSON.stringify(user));
+    console.log('Stored token:', localStorage.getItem("Token"));
   } catch (error) {
     console.error("Login error:", error);
     res.status(500).json({ message: "Server error" });
