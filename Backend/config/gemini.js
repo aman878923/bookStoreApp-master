f17ea9433b9481334a4e726a79c59c4dd9503744
aman agarwal <158491609+aman878923@users.noreply.github.com>
2025-02-26 20:37:47 +0530

    Update gemini.js

--- Backend/config/gemini.js
+++ Backend/config/gemini.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export const geminiModel = genAI.getGenerativeModel({
-  model: "gemini-pro",
+  model: "gemini-2.0-flash",
   generationConfig: {
     maxOutputTokens: 200, // Adjust this number to control response length
   },
